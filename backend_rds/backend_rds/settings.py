a6b08ce5f1947d48558f18987e38984e18ae89c4
vgodun <vgodunsoftware@gmail.com>
2025-05-23 00:17:21 +0300

    init

--- backend_rds/backend_rds/settings.py
+++ backend_rds/backend_rds/settings.py
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'backend_rds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
